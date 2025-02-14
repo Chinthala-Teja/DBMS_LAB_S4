b67eee24b9fdb57588048761b9e0683c24fff96d
Teja Chinthala <chinthalateja17@gmail.com>
2025-02-14 15:39:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,27 +7,9 @@
 
 int main(int argc, char *argv[]) {
   Disk disk_run;
+  // StaticBuffer buffer;
+  // OpenRelTable cache;
 
-  unsigned char buf1[BLOCK_SIZE];  
-  Disk::readBlock( buf1, 4738);
-  char msg1[] = "DBMS_LAB";
-  memcpy(buf1 + 90, msg1, 9);
+  return FrontendInterface::handleFrontend(argc, argv);
   
-  Disk::writeBlock(buf1, 4738);
-
-  unsigned char buf2[BLOCK_SIZE];
-  Disk::readBlock( buf2, 4738);
-  char msg2[9];
-  memcpy(msg2, buf2 + 90, 9);
-  std::cout << msg2;
-
-  //Exercise
-    unsigned char buff[BLOCK_SIZE];
-
- Disk::readBlock( buff , 0);
-  printf("\n");
-  for(int i = 0; i<10; i++)
-      printf("%d ",buff[i]);
-  printf("\n");
-  return 0;
 }
