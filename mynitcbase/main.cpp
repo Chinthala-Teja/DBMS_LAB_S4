4313898f1349c2652de621fe26b3da08b91bc954
Teja Chinthala <chinthalateja17@gmail.com>
2025-02-14 15:27:54 +0530

    Update main.cpp

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -2,21 +2,32 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
+#include<iostream>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   Disk disk_run;
 
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
+  unsigned char buf1[BLOCK_SIZE];  
+  Disk::readBlock( buf1, 4738);
+  char msg1[] = "DBMS_LAB";
+  memcpy(buf1 + 90, msg1, 9);
+  
+  Disk::writeBlock(buf1, 4738);
 
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  std::cout << message2;
+  unsigned char buf2[BLOCK_SIZE];
+  Disk::readBlock( buf2, 4738);
+  char msg2[9];
+  memcpy(msg2, buf2 + 90, 9);
+  std::cout << msg2;
 
+  //Exercise
+    unsigned char buff[BLOCK_SIZE];
+
+ Disk::readBlock( buff , 0);
+  printf("\n");
+  for(int i = 0; i<10; i++)
+      printf("%d ",buff[i]);
+  printf("\n");
   return 0;
 }
