47da5f47359a6a7761dc7060b68a4c28acee3367
Teja Chinthala <chinthalateja17@gmail.com>
2025-02-19 18:50:30 +0530

    Stage - 3

--- main.cpp
+++ main.cpp
@@ -7,9 +7,30 @@
 
 int main(int argc, char *argv[]) {
   Disk disk_run;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
+ StaticBuffer buffer;
+ OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
-  
+  //return FrontendInterface::handleFrontend(argc, argv);
+
+  for(int i = 0; i <= 2; i++){
+
+    RelCatEntry relCatBuf;
+    RelCacheTable::getRelCatEntry(i, &relCatBuf);
+    printf("Relation : %s\n", relCatBuf.relName);
+
+    for(int j = 0; j < relCatBuf.numAttrs; j++){
+
+    AttrCatEntry attrCatBuf;
+    AttrCacheTable::getAttrCatEntry(i, j, &attrCatBuf);
+
+    const char *attrType = attrCatBuf.attrType == NUMBER ? "NUM" : "STR";
+
+    printf(" %s: %s\n", attrCatBuf.attrName, attrType);
+
+    }
+    printf("\n");
+
+  }
+
+  return 0;
 }
