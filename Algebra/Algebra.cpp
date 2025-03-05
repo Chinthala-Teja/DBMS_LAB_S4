e746f816ea4904ad6d94767f31486facbe7aff46
Teja Chinthala <chinthalateja17@gmail.com>
2025-03-05 18:39:56 +0530

    Stage - 7

--- Algebra/Algebra.cpp
+++ Algebra/Algebra.cpp
@@ -75,7 +75,7 @@ int Algebra::select(char srcRel[ATTR_SIZE], char targetRel[ATTR_SIZE], char attr
     
           // get the record at searchRes using BlockBuffer.getRecord
             RecBuffer BlockBuffer(searchRes.block);
-                AttrCatEntry attrCatEntry;
+            AttrCatEntry attrCatEntry;
 
       
             Attribute rec[relCatEntry.numAttrs];
@@ -104,5 +104,48 @@ int Algebra::select(char srcRel[ATTR_SIZE], char targetRel[ATTR_SIZE], char attr
       return SUCCESS;
 }
 
+// STAGE - 7
+int Algebra::insert(char relName[ATTR_SIZE], int nAttrs, char record[][ATTR_SIZE]){
+
+    if(strcmp(relName,"RELATIONCAT") == 0 || strcmp(relName, "ATTRIBUTECAT") == 0)
+        return E_NOTPERMITTED;
+    
+    int relId = OpenRelTable::getRelId(relName);
+
+    if(relId == E_RELNOTOPEN)
+      return E_RELNOTOPEN;
+    
+    RelCatEntry relCatBuf;
+    RelCacheTable::getRelCatEntry(relId, &relCatBuf);
+
+    if(relCatBuf.numAttrs != nAttrs)
+      return E_NATTRMISMATCH;
+    
+    union Attribute recordValues[nAttrs];
+
+    for(int i = 0; i < nAttrs; i++){
+
+      AttrCatEntry attrCatEntry;
+      
+      AttrCacheTable::getAttrCatEntry(relId, i, &attrCatEntry);
+      
+      int type = attrCatEntry.attrType;
+
+      if(type == NUMBER){
+
+        if(isNumber(record[i])){
+          recordValues[i].nVal = atof(record[i]);
+        }
+        else  
+          return E_ATTRTYPEMISMATCH;
+      }
+      else if( type == STRING)
+          strcpy(recordValues[i].sVal,record[i]);
+    }
+
+   int retVal =  BlockAccess::insert(relId, recordValues);
+
+   return retVal;
+}
 
 
