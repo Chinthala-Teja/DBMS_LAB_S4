a17efeb73ecf7d2a309147ab5254e137b47ee4ac
Teja Chinthala <chinthalateja17@gmail.com>
2025-03-26 11:35:36 +0530

    Stage - 9

--- Frontend/Frontend.cpp
+++ Frontend/Frontend.cpp
@@ -11,7 +11,6 @@ int Frontend::drop_table(char relname[ATTR_SIZE]) {
   return Schema::deleteRel(relname);
 }
 
-
 int Frontend::open_table(char relname[ATTR_SIZE]) {
   // Schema::openRel
   return Schema::openRel(relname);
@@ -44,35 +43,74 @@ int Frontend::drop_index(char relname[ATTR_SIZE], char attrname[ATTR_SIZE]) {
 }
 
 int Frontend::insert_into_table_values(char relname[ATTR_SIZE], int attr_count, char attr_values[][ATTR_SIZE]) {
-  // Algebra::insert
   return Algebra::insert(relname, attr_count, attr_values);
 }
 
 int Frontend::select_from_table(char relname_source[ATTR_SIZE], char relname_target[ATTR_SIZE]) {
-  // Algebra::project
-  return SUCCESS;
+
+  // Call appropriate project() method of the Algebra Layer
+
+  // Return Success or Error values appropriately
+  return Algebra::project(relname_source, relname_target);
 }
 
-int Frontend::select_attrlist_from_table(char relname_source[ATTR_SIZE], char relname_target[ATTR_SIZE],
-                                         int attr_count, char attr_list[][ATTR_SIZE]) {
-  // Algebra::project
-  return SUCCESS;
+int Frontend::select_attrlist_from_table(char relname_source[ATTR_SIZE],
+  char relname_target[ATTR_SIZE],
+  int attr_count,
+  char attr_list[][ATTR_SIZE]) {
 
+// Call appropriate project() method of the Algebra Layer
+
+// Return Success or Error values appropriately
+return Algebra::project(relname_source, relname_target, attr_count, attr_list);
 }
 
 int Frontend::select_from_table_where(char relname_source[ATTR_SIZE], char relname_target[ATTR_SIZE],
                                       char attribute[ATTR_SIZE], int op, char value[ATTR_SIZE]) {
   // Algebra::select
-
-   return Algebra::select(relname_source, relname_target, attribute, op, value);
-  ;
+  return Algebra::select(relname_source, relname_target, attribute, op, value);
+  //return SUCCESS;
 }
 
-int Frontend::select_attrlist_from_table_where(char relname_source[ATTR_SIZE], char relname_target[ATTR_SIZE],
-                                               int attr_count, char attr_list[][ATTR_SIZE],
-                                               char attribute[ATTR_SIZE], int op, char value[ATTR_SIZE]) {
-  // Algebra::select + Algebra::project??
-  return SUCCESS;
+int Frontend::select_attrlist_from_table_where(
+  char relname_source[ATTR_SIZE], char relname_target[ATTR_SIZE],
+  int attr_count, char attr_list[][ATTR_SIZE],
+  char attribute[ATTR_SIZE], int op, char value[ATTR_SIZE]) {
+
+    char temp[] = ".temp";  // Create a mutable copy of the string
+
+
+  // Call select() method of the Algebra Layer with correct arguments to
+  // create a temporary target relation with name ".temp" (use constant TEMP)
+  int k = Algebra::select(relname_source, temp, attribute, op, value);
+
+  // TEMP will contain all the attributes of the source relation as it is the
+  // result of a select operation
+
+  // Return Error values, if not successful
+  if(k!=SUCCESS) return k;
+
+  // Open the TEMP relation using OpenRelTable::openRel()
+  // if open fails, delete TEMP relation using Schema::deleteRel() and
+  // return the error code
+  int z = OpenRelTable::openRel(temp);
+  if(z<0 && z>=MAX_OPEN){
+    Schema::deleteRel(temp);
+    return z;
+  } 
+
+  // On the TEMP relation, call project() method of the Algebra Layer with
+  // correct arguments to create the actual target relation. The final
+  // target relation contains only those attributes mentioned in attr_list
+  int zx = Algebra::project(temp, relname_target, attr_count, attr_list);
+
+  // close the TEMP relation using OpenRelTable::closeRel()
+  OpenRelTable::closeRel(z);
+  // delete the TEMP relation using Schema::deleteRel()
+  Schema::deleteRel(temp);
+
+  // return any error codes from project() or SUCCESS otherwise
+  return zx;
 }
 
 int Frontend::select_from_join_where(char relname_source_one[ATTR_SIZE], char relname_source_two[ATTR_SIZE],
